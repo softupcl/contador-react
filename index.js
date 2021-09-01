ce3af6bcc70d16fd273573f23c4fb33b7139a5de
softupcl <81437285+softupcl@users.noreply.github.com>
2021-09-01 15:27:56 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,1 @@
+ReactDOM.render(<Contador></Contador>, document.querySelector("#root"));
---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_ELEMENTS = [
+    "./",
+    "https://unpkg.com/react@17/umd/react.production.min.js",
+    "https://unpkg.com/react-dom@17/umd/react-dom.production.min.js",
+    "https://unpkg.com/@babel/standalone/babel.min.js",
+    "./style.css",
+    "./components/Contador.js"
+]
+
+const CACHE_NAME = "v2_cache_contador_react";
+
+//Instala el servir worker
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            cache.addAll(CACHE_ELEMENTS).then(() => {
+                self.skipWaiting();
+            }).catch(err => console.log(err));
+        })
+    );
+});
+
+//Actctiva
+self.addEventListener("activate", (e) => {
+    const cacheWhiteList = [CACHE_NAME];
+    e.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(cacheNames.map(cacheName => {
+                return cacheWhiteList.indexOf(cacheName) === -1 && caches.delete(cacheName)
+            }))
+        }).then(() => self.clients.claim())
+    );
+});
+
+
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        caches.match(e.request).then((res) => {
+            if (res) {
+                return res;
+            }
+            return fetch(e.request);
+        })
+    );
+});
